83bb39de6692c272eccf25f63b5f4fefde4fe28b
arielglvz <arielglvz@gmail.com>
2025-01-27 17:56:37 +0800

    added UI and integrated the firestore/storage for the backend

--- frontend/src/pages/client/Technology/index.jsx
+++ frontend/src/pages/client/Technology/index.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { textDB } from '../../../firebaseConfig';
 import Title from '../../../components/Title'
-import { techStack } from '../../../StaticData'
-import { Icon } from '@iconify/react';
+// import { techStack } from '../../../StaticData'
+// import { Icon } from '@iconify/react';
 import './technology.scss'
+import { collection, getDocs } from 'firebase/firestore';
 
 const Technology = () => {
+  const [fileList, setFileList] = useState([])
+
+  const sortedArray = fileList.sort((a, b) => a.id - b.id)
+
+  const getData = async () => { 
+    try {
+      const valRef = collection(textDB, 'teckstack'); // Replace 'txtData' with your collection name
+      const dataDB = await getDocs(valRef);
+      const allData = dataDB.docs.map((doc) => doc.data());
+      setFileList(allData);
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
     <div className='technology container'>
       <Title 
@@ -12,12 +33,15 @@ const Technology = () => {
         subtitle='Technologies I&#x0027;ve been working with recently'
       />
       <div className="technology__tech-list">
-        {techStack.map((tech, index) => (
-          <div className="technology__tech-item" key={index}>
-            <img src={tech.icon} alt="" />
-            {/* <p>{tech.name}</p> */}
-          </div>
-        ))}
+        {fileList.length > 0 ? (
+          sortedArray.map((item) => (
+            <div key={item.id} className="technology__tech-item" >
+              <img src={item.imageURL} alt={item.labelVal} />
+            </div>
+          ))
+        ) : (
+          <p>No data available</p>
+        )}
       </div>
     </div>
   )
